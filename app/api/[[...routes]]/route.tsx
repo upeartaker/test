f04e814af73fd5f1b905453f6b0cd60e5bf5762f
upeartaker <upeartaker0@gmail.com>
2024-07-26 19:54:04 +0800

    initialize

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,85 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  title: 'Frog Frame',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    imageOptions: {
+      format: 'svg',
+    },
+    image: (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+          fontSize: 32,
+          fontWeight: 600,
+        }}
+      >
+        <svg
+          width='75'
+          viewBox='0 0 75 65'
+          fill='#000'
+          style={{ margin: '0 75px' }}
+        >
+          <path d='M37.59.25l36.95 64H.64l36.95-64z'></path>
+        </svg>
+        <div style={{ marginTop: 40 }}>Hello, World</div>
+        <img
+          style={{
+            width: 100,
+          }}
+          src='https://github.githubassets.com/assets/mona-loading-default-c3c7aad1282f.gif'
+        />
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder='Enter custom fruit...' />,
+      <Button value='apples'>Apples</Button>,
+      <Button value='oranges'>Oranges</Button>,
+      <Button value='bananas'>Bananas</Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
+
+// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
+// static assets to the public folder. You can do this by adding a script to your package.json:
+// ```json
+// {
+//   scripts: {
+//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
+//   }
+// }
+// ```
+// Next, you'll want to set up the devtools to use the correct assets path:
+// ```ts
+// devtools(app, { assetsPath: '/.frog' })
+// ```
